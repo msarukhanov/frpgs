b94740a0098b52d994fc8379439bccf20acbf132
msarukhanov <mark.sarukhanov@cup.games>
2025-02-02 14:11:00 +0400

    init

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const WebSocketServer = require('ws');
-// Creating a new websocket server
+
 const wss = new WebSocketServer.Server({ port: 8080 });
-// Creating connection using websocket
+
 wss.on("connection", ws => {
 
     console.log("new client connected");
 
-    ws.send('Welcome, you are connected!');
-
     ws.on("message", data => {
-        console.log(`Client has sent us: ${data}`)
+        console.log(`Client has sent us: ${data}`);
+        ws.send(data);
     });
 
     ws.on("close", () => {
-        console.log("the client has connected");
+        console.log("the client has disconnected");
     });
 
     ws.onerror = function () {
         console.log("Some Error occurred")
-    }
+    };
 });
 console.log("The WebSocket server is running on port 8080");
