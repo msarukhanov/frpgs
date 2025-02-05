6de3bac02a2148ff1926485e5210f4db57406c38
msarukhanov <mark.sarukhanov@cup.games>
2025-02-05 17:40:44 +0400

    port change

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const WebSocketServer = require('ws');
-const port = 443;
-const wss = new WebSocketServer.Server({ port });
+const portWss = 443, portWs = 8080;
+const wssServer = new WebSocketServer.Server({ portWss });
+const wsServer = new WebSocketServer.Server({ portWs });
 
 const channels = {
     chat : []
 };
 
-wss.on('connection', ws => {
+wsServer.on('connection', ws => {
 
-    console.log('new client connected');
+    console.log('new client connected WS');
 
     channels['chat'].push(ws);
     ws.send('joined');
@@ -27,4 +28,29 @@ wss.on('connection', ws => {
         console.log('Some Error occurred')
     };
 });
-console.log('The WebSocket server is running on port '+port);
+
+wssServer.on('connection', ws => {
+
+    console.log('new client connected WSS');
+
+    channels['chat'].push(ws);
+    ws.send('joined');
+
+    ws.on('message', data => {
+        console.log(`Client has sent us: ${data}`);
+        ws.send(data);
+    });
+
+    ws.on('close', () => {
+        console.log('the client has disconnected');
+        channels['chat'] = channels['chat'].filter((client) => client !== ws);
+    });
+
+    ws.onerror = function () {
+        console.log('Some Error occurred')
+    };
+});
+
+console.log('The WebSocket server wsServer is running on port '+wsServer);
+
+console.log('The WebSocket server wssServer is running on port '+wssServer);
