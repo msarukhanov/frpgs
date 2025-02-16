ee3457d529d0422e239c9dc5c57f9362dd4295fb
msarukhanov <mark.sarukhanov@cup.games>
2025-02-16 12:47:32 +0400

    pl char changes

--- src/controllers/app/index.js
+++ src/controllers/app/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 router.use('/characters', require('./characters.controller'));
 router.use('/chat', require('./chat.controller'));
+router.use('/maps', require('./maps.controller'));
+router.use('/parties', require('./parties.controller'));
 router.use('/religions', require('./religions.controller'));
 router.use('/seasons', require('./seasons.controller'));
 router.use('/users', require('./users.controller'));
---
+++ src/controllers/app/maps.controller.js
@@ -0,0 +1,22 @@
+﻿const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+router.get('/', list);
+router.post('/', save);
+
+function list(req, res, next) {
+    fs.readFile(__dirname + '/../../static/map.json', 'utf-8', function(err, data){
+        if (err) res.json({err:true});
+        res.json(JSON.parse(data));
+    });
+}
+
+function save(req, res, next) {
+    fs.writeFile(__dirname + '/../../static/map.json', JSON.stringify(req.body), 'utf-8', function (err) {
+        if (err) res.json({err:true});
+        res.json(1);
+    });
+}
+
+module.exports = router;
