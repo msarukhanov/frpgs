0f0586c1a0a782ebff79255700d123c1ca8c6fdd
msarukhanov <mark.sarukhanov@cup.games>
2025-02-18 00:51:12 +0400

    map local changes

--- src/controllers/app/maps.controller.js
+++ src/controllers/app/maps.controller.js
@@ -2,21 +2,57 @@
 const router = express.Router();
 const fs = require('fs');
 
-router.get('/', list);
-router.post('/', save);
+router.get('/', getView);
+router.post('/', saveView);
+router.get('/edit/', getEdit);
+router.post('/edit/', saveEdit);
 
-function list(req, res, next) {
-    fs.readFile(__dirname + '/../../static/map.json', 'utf-8', function(err, data){
-        if (err) res.json({err:true});
-        res.json(JSON.parse(data));
-    });
+function getView(req, res, next) {
+    try {
+        fs.readFile(__dirname + '/../../static/maps/world.json', 'utf-8', function (err, data) {
+            if (err) res.json({err: true});
+            res.json(JSON.parse(data));
+        });
+    }
+    catch (e) {
+        res.json(JSON.parse({err: true}))
+    }
 }
 
-function save(req, res, next) {
-    fs.writeFile(__dirname + '/../../static/map.json', JSON.stringify(req.body), 'utf-8', function (err) {
-        if (err) res.json({err:true});
-        res.json(1);
-    });
+function saveView(req, res, next) {
+    try {
+        fs.writeFile(__dirname + '/../../static/maps/world.json', JSON.stringify(req.body), 'utf-8', function (err) {
+            if (err) res.json({err: true});
+            res.json(1);
+        });
+    }
+    catch (e) {
+        res.json(JSON.parse({err: true}))
+    }
+}
+
+function getEdit(req, res, next) {
+    try {
+        fs.readFile(__dirname + '/../../static/map.json', 'utf-8', function (err, data) {
+            if (err) res.json({err: true});
+            res.json(JSON.parse(data));
+        });
+    }
+    catch (e) {
+        res.json(JSON.parse({err: true}))
+    }
+}
+
+function saveEdit(req, res, next) {
+    try {
+        fs.writeFile(__dirname + '/../../static/map.json', JSON.stringify(req.body), 'utf-8', function (err) {
+            if (err) res.json({err: true});
+            res.json(1);
+        });
+    }
+    catch (e) {
+        res.json(JSON.parse({err: true}))
+    }
 }
 
 module.exports = router;
